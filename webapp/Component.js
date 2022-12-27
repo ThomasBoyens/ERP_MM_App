e4a72f3ad953dc5b9fee48ff95d3e14c3e23b7ac
ThomasB <thomas.boyens@hotmail.com>
2022-12-27 16:23:49 +0100

    repo init

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zsd041mmdata2.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
